3f465d2ae71eea2e0ef00e9a13b254d8fae68be4
Gisele <141287468+giseleoliver9@users.noreply.github.com>
2025-02-04 14:00:25 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+let amigos = [];
+
+function adicionarAmigo() {
+    let input = document.getElementById("amigo");
+    let nome = input.value.trim();
+    
+    if (nome === "") {
+        alert("Por favor, insira um nome.");
+        return;
+    }
+    
+    amigos.push(nome);
+    
+    input.value = "";
+    
+    atualizarLista();
+}
+
+function atualizarLista() {
+    let lista = document.getElementById("listaAmigos");
+    
+    lista.innerHTML = "";
+    
+    amigos.forEach(amigo => {
+        let li = document.createElement("li");
+        li.textContent = amigo;
+        lista.appendChild(li);
+    });
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("Adicione pelo menos um nome antes de sortear.");
+        return;
+    }
+    
+    let indiceSorteado = Math.floor(Math.random() * amigos.length);
+    
+    let amigoSorteado = amigos[indiceSorteado];
+    
+    document.getElementById("resultado").innerHTML = `<li>Amigo Secreto: ${amigoSorteado}</li>`;
+}
